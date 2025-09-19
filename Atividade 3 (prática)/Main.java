1eae379ef38b92201c5098c3600e8530546cd99f
𝓛𝓲𝓿 ₍^. .^₎Ⳋ🎐 <yliviah2021@gmail.com>
2025-09-19 11:10:08 -0300

    Add files via upload

---
+++ Atividade 3 (prática)/Main.java
@@ -0,0 +1,24 @@
+// Crie um código para uma loja, onde tenha o nome dos clientes e pelo menos duas informações básicas sobre eles. (Em bloco) 
+public class Main {
+    public static void main(String[] args) {
+        // 1. Criando um objeto da classe Cliente
+        Cliente oCliente = new Cliente("Aron", 333666999);
+
+        // 2. Chamando os métodos do objeto 'oCliente'
+        System.out.println("--- Informações do Cliente ---");
+        oCliente.exibirInfo();
+        oCliente.pagar();
+        System.out.println("O nome do cliente é: " + oCliente.getNome());
+
+        System.out.println("\n----------------------------\n");
+
+        // 3. Criando um objeto da classe Gerente
+        Gerente oGerente = new Gerente("Lívia", "Tons de Roxo");
+
+        // 4. Chamando os métodos do objeto 'oGerente'
+        System.out.println("--- Informações do Gerente ---");
+        oGerente.exibirInfo();
+        oGerente.reclamar();
+        System.out.println("O nome da loja é: " + oGerente.getNome_loja());
+    }
+} 
